518d423f87b1c6087e5289d8b252800f510f402a
devangel112 <dev.angel.perez@gmail.com>
2022-07-06 19:44:59 -0500

    Vue project uploaded

---
+++ src/router/index.ts
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/HomeView.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
